48ef9af12fb5a1737ff9488e8577b968227bbfab
DP <1023279358@qq.com>
2019-01-30 09:16:50 +0800

    130916

--- user/vs_detail.py
+++ user/vs_detail.py
@@ -6,18 +6,32 @@ vs_detail_blue = Blueprint('vs_detail1',__name__,template_folder='user_templates
 
 @vs_detail_blue.route('/user/vs_detail/')
 def vs_detail1():
+    print('success')
     return render_template('video_detail.html')
 
 @vs_detail_blue.route('/user/video_test/',endpoint='video_test')
 def test():
   # flask.send_file()
-  return send_from_directory('user/user_static/', '123.avi', as_attachment=True)
+
+    # # print('video')
+    # with open('user/user_static/321.mp4') as f:
+    #     print('vaideo',f.read())
+
+  # return 
+    return send_from_directory('user/user_static/', '123.mp4', as_attachment=True)
+
+@vs_detail_blue.route('/user/video_img/',endpoint='video_img')
+def videoimg():
+    print('img')
+    return send_from_directory('user/user_static/', '2.jpg', as_attachment=True)
 
 
 def gen(camera):
-  while True:
+  n=0
+  while n<10:
     frame = camera.get_frame()
-    print('laji',frame)
+    # print('laji',frame)
+    n+=1
     yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
   
@@ -25,6 +39,8 @@ def gen(camera):
 def video_feed():
     print('视屏过来了')
     return Response(gen(Camera()),mimetype='multipart/x-mixed-replace; boundary=frame')
+  
+
 
 class Camera(object):
   def __init__(self):
